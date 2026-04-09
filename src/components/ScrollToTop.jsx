ddce736651212920fecf4b2e053178763540e878
muhammadawais00900 <muhammadawais09956@gmail.com>
2026-04-09 09:39:24 +0545

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,13 +1,14 @@
-
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [pathname]);
+    window.scrollTo(0, 0);
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }, [pathname, search]);
 
   return null;
 };
